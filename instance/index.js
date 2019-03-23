c8fcb8ce4953c54f208a191bc8c5fb3cf4eca008
GoSubRoutine <erksonbr@yahoo.com>
2019-03-23 14:55:33 -0300

    then

--- instance/index.js
+++ instance/index.js
@@ -1,3 +1,3 @@
 "use strict";
-console.log(import("./sketch.js"));
+console.log(import("./sketch.js").then(m => console.log(m)));
 new p5(sketch);
--- instance/index.ts
+++ instance/index.ts
@@ -1,2 +1,2 @@
-console.log(import("./sketch.js"));
+console.log(import("./sketch.js").then(m => console.log(m)));
 new p5(sketch);
